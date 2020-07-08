a1dde9593d3aa00e004d0c9f5fe1515218a8a966
elenamlopez <elena.mateos1@gmail.com>
2020-07-08 20:31:58 +0200

    Basic CRUD

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 const axios = require('axios');
 const port = 3000;
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false })); // Parsea lo que venga en el body
+
+app.use(bodyParser.json()); // Parsea lo que enviamos nostotros
 
 // CRUD
 app.get('/', async (req, res) => {
@@ -11,6 +16,37 @@ app.get('/', async (req, res) => {
   res.send(data);
 });
 
-app.listen(3000, function () {
+/* Post */
+app.post('/', async (req, res) => {
+  const { body } = req;
+  const { data } = await axios.post(
+    'https://jsonplaceholder.typicode.com/users',
+    body
+  );
+  res.status(201).send(data);
+});
+
+/* Put */
+
+app.put('/:id', async (req, res) => {
+  const { body } = req;
+  const { id } = req.params;
+  const { data } = await axios.put(
+    `https://jsonplaceholder.typicode.com/users/${id}`,
+    body
+  );
+  res.sendStatus(204);
+});
+
+/* Dlelete */
+app.delete('/:id', async (req, res) => {
+  const { id } = req.params;
+  const { data } = await axios.delete(
+    `https://jsonplaceholder.typicode.com/users/${id}`
+  );
+  res.sendStatus(204);
+});
+
+app.listen(port, function () {
   console.log(`Example app listening on port ${port}!`);
 });
