797af811846f6fd3fd9572489936a784623b1ad6
elenamlopez <elena.mateos1@gmail.com>
2020-07-09 13:32:50 +0200

    First test, dummie test

---
+++ index.spec.js
@@ -0,0 +1,10 @@
+describe('Prueba', () => {
+  describe('suma', () => {
+    it('suma dos números', () => {
+      const suma = (a, b) => {
+        return a + b;
+      };
+      expect(suma(1, 2)).toEqual(3);
+    });
+  });
+});
